1660c957f6fb6efb7ee79ac41e7042f14949038f
Brian Leonard <brian@bleonard.com>
2021-03-02 17:46:27 -0800

    Better to give real error if Mailchimp invalid

--- grouparoo/config/apps/mailchimp.js
+++ grouparoo/config/apps/mailchimp.js
@@ -1,10 +1,5 @@
 exports.default = async function buildConfig() {
   const apiKey = process.env.MAILCHIMP_API_KEY;
-  if (!apiKey || apiKey === "632d084f1bb6ff63e8336bd4864373ed-us3") {
-    // hasn't really set it, don't set it up
-    console.log("Set MAILCHIMP_API_KEY to sync to Mailchimp.");
-    return [];
-  }
 
   return [
     {
--- grouparoo/config/synctable/sync_mailchimp.js
+++ grouparoo/config/synctable/sync_mailchimp.js
@@ -2,12 +2,6 @@ module.exports = async function getConfig() {
   const mailchimpListId = process.env.MAILCHIMP_LIST_ID;
   const tableName = "sync_mailchimp";
 
-  if (!mailchimpListId || mailchimpListId === "26d8e9db1e") {
-    // hasn't really set it, don't set it up
-    console.log("Set MAILCHIMP_LIST_ID to sync to Mailchimp.");
-    return [];
-  }
-
   // single entity to define a source and destination
   // optimizes for the case where they are transforming a data warehouse into the shape of data to sync
   return [
